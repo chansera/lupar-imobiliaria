bf81e7d64a9e318f8ab2f0ed710a1b4011135788
Csrand <chrystiansenna@gmail.com>
2026-01-28 00:14:26 -0300

    feat: Responsive adjusts on Header
    	modified:   site/src/lib/components/organisms/Header.svelte
        Responsive sizes to header on large interfaces

--- site/src/lib/components/organisms/Footer.svelte
+++ site/src/lib/components/organisms/Footer.svelte
@@ -57,7 +57,7 @@
         </li>
         <li class="flex items-start gap-2">
           <svg class="text-brand mt-1" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z"/><circle cx="12" cy="10" r="3"/></svg>
-          <span>Av. Exemplo, 123<br/>Birigui - SP</span>
+          <span>Av. Exemplo, 123<br/>Três Lagoas - MS</span>
         </li>
       </ul>
     </div>
